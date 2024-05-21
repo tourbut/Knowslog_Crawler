a3c064f03f49aca788e8b2f826cf825d44b43d72
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-05-21 18:13:32 +0900

    Refactored parse_dom function and added support for parsing link and image tags

--- get_webpage.py
+++ get_webpage.py
@@ -22,13 +22,22 @@ def parse_dom(node):
     parsed_content = []
 
     if isinstance(node, Tag):
-        if node.name not in ["script", "style",'html','meta','head','body']:
-            parsed_content.append(node.text.replace('\n',' ').strip())
-        else:
+        #print(node.name)
+        if node.name in ["script", "style",'html','meta','head','body','div','main','figure','header','article']:
             for child in node.children:
                 child_content = parse_dom(child)
                 if child_content:
                     parsed_content.append(child_content)
+        elif node.name == "link":
+            href = node.get('href')
+            if href and href.startswith("http"):
+                parsed_content.append(f"[Link]({href.strip()})\n")
+        elif node.name == "img":
+            src = node.get('src')
+            if src:
+                parsed_content.append(f"![Image]({src.strip()})\n")
+        else:
+            parsed_content.append(node.text.replace('\n',' ').strip())
     
     if parsed_content:
         return '\n'.join(parsed_content)
@@ -37,8 +46,6 @@ def parse_dom(node):
 
 suggestions = DDGS().suggestions("Latest AI News",region="us-en")
 #print(suggestions)
-answers = DDGS().answers('sun')
-print(answers)
 results = DDGS().news(keywords=f"{suggestions[0]['phrase']} -site:msn.com", 
                       region="us-en",
                       safesearch="moderate", 
@@ -46,7 +53,7 @@ results = DDGS().news(keywords=f"{suggestions[0]['phrase']} -site:msn.com",
                       max_results=20)
 
 #print(results)
-url = results[0]['url']
+url = 'https://www.deeplearning.ai/the-batch/issue-249/'
 dom = get_dom(url)
 contents = get_contents(dom)
 print(contents)
