21ea580fe3a49fdb301e9f3a3c5013f45600d654
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-24 17:35:31 +0900

    Add user-related tables and CRUD operations

--- backend/app/src/crud/users.py
+++ backend/app/src/crud/users.py
@@ -26,3 +26,40 @@ async def acreate_user(*, session: Session, user_create: user_schema.UserCreate)
     await session.commit()
     await session.refresh(db_obj)
     return db_obj
+
+
+async def get_detail(*, session: Session, user_id: int) -> UserDetail | None:
+    statement = select(UserDetail).where(UserDetail.user_id == user_id)
+    session_user = await session.exec(statement)
+    return session_user.first()
+
+async def create_detail(*, session: Session, user_detail: user_schema.UserDetail, user_id: int) -> UserDetail:
+    
+    db_obj = UserDetail.model_validate(user_detail, update={"user_id": user_id})
+    
+    session.add(db_obj)
+    await session.commit()
+    await session.refresh(db_obj)
+    return db_obj
+
+async def update_detail(*, session: Session, user_detail: user_schema.UserDetail, user_id: int) -> UserDetail:
+    db_obj = UserDetail.model_validate(user_detail, update={"user_id": user_id})
+    detail = await get_detail(session=session, user_id=user_id)
+    
+    if not detail:
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+    else:
+        detail.name = db_obj.name
+        detail.age = db_obj.age
+        detail.discord_yn = db_obj.discord_yn
+        detail.email_yn = db_obj.email_yn
+        detail.llm_model = db_obj.llm_model
+        detail.api_key = db_obj.api_key
+        detail.interests = db_obj.interests
+        detail.update_date = datetime.now()
+
+        await session.commit()
+    
+    return db_obj
