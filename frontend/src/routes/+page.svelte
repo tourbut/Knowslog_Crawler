685ed661acab1a513fe6d4feec30390394426a83
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-25 15:34:03 +0900

    Update frontend dependencies and styles

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script>
-    import { goto } from '$app/navigation';
-	  import { login } from '$lib/apis/user';
-    import Error from "$lib/components/Error.svelte";
-    import { user_token,username } from '$lib/stores';
-
-
-
-  </script>
-  {#if $username}
-    <div class="container mx-auto p-4 max-w-md">
-      <h1>{$username}님 반갑습니다.</h1>
-    </div>
-  {/if}
+    import { username } from '$lib/stores';
+  import { Alert } from 'flowbite-svelte';
+</script>
+{#if $username}
+  <div class="container mx-auto p-4 max-w-md">
+    <h1>{$username}님 반갑습니다.</h1>
+  </div>
+{/if}
