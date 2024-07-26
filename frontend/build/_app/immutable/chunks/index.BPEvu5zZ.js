a777c74373916c291710b18b545e145dedee6b79
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-26 09:04:44 +0900

    npm build

--- frontend/build/_app/immutable/chunks/index.BPEvu5zZ.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./index.-PiOOl_e.js";const c="Knowslog Crawler",A="",u=`${A}/api/v1`,s=(t,r)=>{const e=sessionStorage.getItem(t),o=a(e!=null?JSON.parse(e):r);return o.subscribe(n=>{sessionStorage.setItem(t,JSON.stringify(n))}),o},_=s("APP_NAME",c),S=s("user_token",""),g=s("username","");export{_ as A,S as a,u as b,g as u};
--- frontend/build/_app/immutable/entry/start.BGy76tRm.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.C0MaJTiG.js";export{t as start};
