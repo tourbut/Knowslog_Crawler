aa226fa31e199ed347ba3c5fb8b92f404fd6a967
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-26 13:28:35 +0900

    Remove .md extension from svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
   
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte'],
 	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
