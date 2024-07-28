e5cec48a21ff0cefe91492ae3715c472cd16d18e
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-28 23:22:32 +0900

    feat: Add footer component to common components

---
+++ frontend/src/lib/components/common/Footer.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Footer, FooterCopyright, FooterLinkGroup, FooterBrand, FooterLink } from 'flowbite-svelte';
+</script>
+
+<Footer footerType="logo">
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <FooterCopyright href="/" by="Knowslog" />
+</Footer>
