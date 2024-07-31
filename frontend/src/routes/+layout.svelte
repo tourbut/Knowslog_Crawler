e7946468a30921c35c6c55e1388f2c8aedda5f31
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-31 17:03:05 +0900

    Add session expiration handling and redirect to login page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import '../app.css';
   import Navigation from '$lib/components/common/Navigation.svelte';
   import Footer from '$lib/components/common/Footer.svelte';
-  import { APP_NAME,user_token } from '$lib/stores';
+  import { APP_NAME,user_token,username } from '$lib/stores';
   import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
   import Toasts from '$lib/components/common/Toasts.svelte';
@@ -23,6 +23,11 @@
     }
 
     let failure_callback = (json_error) => {
+        alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+        
+        username.set("");
+        user_token.set("");
+        goto('/login');
     }
 
     await get_user(params,success_callback, failure_callback)
