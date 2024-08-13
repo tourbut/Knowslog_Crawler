33edf2061b06a4cda8d4c0d1a0db3e70ec37e16f
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-08-13 15:14:11 +0900

    Add nodejs and npm installation to Dockerfile, and redirect to login page if user is not logged in in +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
   import { username } from '$lib/stores';
-  import { Alert } from 'flowbite-svelte';
+  import { goto } from '$app/navigation';
 </script>
 {#if $username}
   <div class="container mx-auto p-4 max-w-md">
     <h1>{$username}님 반갑습니다.</h1>
+  </div>
+{:else}
+  <div class="container mx-auto p-4 max-w-md">
+    {goto('/login')}
   </div>
 {/if}
